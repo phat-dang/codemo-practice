a83444a55c2e7aec2d4de9c5b3f6436c2dc45fc9
Phat Dang <dangphat0608@gmail.com>
2025-01-12 13:13:39 +0700

    main: Updated

--- tests/my-first-test.spec.ts
+++ tests/my-first-test.spec.ts
@@ -1,12 +1,13 @@
 import test, { expect } from "@playwright/test"
 
-test('Email should visible', async ({ page }) => {
+test('Email should be visible', async ({ page }) => {
 
-    //Go to Codemo.dev
+    // Go to Codemo.dev
     await page.goto('https://codemo.dev/')
 
-    //Click on About link
+    // Click on the About link
     await page.getByLabel('About').getByRole('link', { name: 'About' }).click()
+
     // Expects page to have a heading with the name of Installation.
     await expect(page.getByRole('link', { name: 'codemo.dev@gmail.com' })).toBeVisible()
 })
